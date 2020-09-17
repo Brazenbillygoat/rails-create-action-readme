f524e328ba4b1109ae554f7db0cebf0793dd5b59
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-09-16 22:19:08 -0400

    Added create, index, show route and create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,5 +11,8 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  # add create method here
+  def create
+    new_post = Post.create(title: params["title"], description: params["description"])
+    redirect_to post_path(new_post)
+  end
 end
